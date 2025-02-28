0dfa888b6fc1f8cd44a0e0d5fdcbdf742ea9a93f
sandip12 <151613355+srian2@users.noreply.github.com>
2025-03-01 00:23:58 +0545

    Add files via upload

--- signup.jsx
+++ signup.jsx
@@ -5,42 +5,35 @@ import "./signup.css";
 const Signup = () => {
     const [formData, setFormData] = useState({
         fullname: "",
-        username: "",
+       
         email: "",
         password: "",
         confirmPassword: "",
     });
-
     const [message, setMessage] = useState("");
-    const navigate = useNavigate(); // Initialize useNavigate
-
+    const navigate = useNavigate(); 
     const handleChange = (e) => {
         setFormData({ ...formData, [e.target.name]: e.target.value });
     };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-    
-        // Check if passwords match
         if (formData.password !== formData.confirmPassword) {
             setMessage("❌ Passwords do not match");
             return;
         }
-    
         try {
             const response = await fetch("http://localhost:3000/api/auth/signup", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({
-                    Fullname: formData.fullname,   // ✅ Ensure correct field names
-                    username: formData.username,
+                    Fullname: formData.fullname,  
+                    
                     Email: formData.email,
                     Password: formData.password
                 }),
             });
-    
             const data = await response.json();
-    
             if (response.ok) {
                 setMessage("✅ Signup successful! Redirecting to login...");
                 setTimeout(() => navigate("/login"), 2000);
@@ -55,6 +48,7 @@ const Signup = () => {
     
 
     return (
+        <div className="signup-wrapper"> 
         <div className="signup-container">
             <form className="signup-form" onSubmit={handleSubmit}>
                 <h1 className="login-title">🐾 AdoptMe</h1>
@@ -65,10 +59,6 @@ const Signup = () => {
                 <div className="form-group">
                     <label>Fullname:</label>
                     <input type="text" name="fullname" placeholder="Enter your name" value={formData.fullname} onChange={handleChange} required />
-                </div>
-                <div className="form-group">
-                    <label>Username:</label>
-                    <input type="text" name="username" placeholder="Choose a username" value={formData.username} onChange={handleChange} required />
                 </div>
                 <div className="form-group">
                     <label>Email:</label>
@@ -87,9 +77,10 @@ const Signup = () => {
                 <p className="login-link"> <a href="/login">Already have an Account?</a></p>
             </form>
         </div>
+        </div>
     );
 };
 
 export default Signup;
 
- 
+
